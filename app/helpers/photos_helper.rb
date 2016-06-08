7f1bb69accf4e39372f3ac7165a347d58ecfd0c5
Kalior <kalioragus@gmail.com>
2016-06-09 00:50:16 +0200

    Add caching for front page photos Don't really know if this is a good idea or not

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,7 @@
 module PhotosHelper
+  def cache_key_for_photos
+    count          = Photo.count
+    max_updated_at = Photo.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "photos/all-#{count}-#{max_updated_at}"
+  end
 end
