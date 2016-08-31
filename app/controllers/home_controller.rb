1ef2688c6d8e9563fc77bf0272e1ec0d2725a60d
Kalior <kalioragus@gmail.com>
2016-08-31 23:48:16 +0200

    Move the reverse operation on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @photos = Photo.all
     @projects = Project.all
-    @combined = (@photos + @projects).sort_by(&:created_at)
+    @combined = (@photos + @projects).sort_by(&:created_at).reverse
   end
 end
