e0616aebea2d82712b4c56df16850dfe2d25d9ae
Kalior <kalioragus@gmail.com>
2017-08-23 16:42:39 +0200

    Upgrade foundation and rails

--- app/models/me.rb
+++ app/models/me.rb
@@ -1,2 +1,2 @@
-class Me < ActiveRecord::Base
+class Me < ApplicationRecord
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
-class Photo < ActiveRecord::Base
+class Photo < ApplicationRecord
   mount_uploader :image, ImageUploader
   #validates :image, presence: true
 end
