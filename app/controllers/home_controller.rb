701e0285832276e3c580e42f74be16fde06e7be4
Kalior <kalioragus@gmail.com>
2017-10-31 10:48:05 +0100

    Update layout of site to use photo_collections

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @photos = Photo.all
+    @photos = PhotoCollection.all
     @projects = Project.all
     @combined = (@photos + @projects).sort_by(&:created_at).reverse
   end
